bfabc8e8827f8ce296a9226ad15c7056df823cf8
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-04-16 21:39:43 -0300

    iniciando pagina de projetos

---
+++ portfolio-web/src/data/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+  {
+    name: 'Netflix Clone',
+    description: 'Clone do netflix usando React Hooks, CSS, e API de filmes.',
+  },
+  {
+    name: 'Loja Online',
+    description: 'Loja Online Desenvolvida usando React-Redux e API do MercadoLivre.',
+  },
+  {
+    name: 'Trybe Wallet',
+    description: 'Carteira de gastos usando dados de API de câmbio em tempo real',
+  },
+];
+
+export default projects;
--- portfolio-web/src/pages/Projects.jsx
+++ portfolio-web/src/pages/Projects.jsx
@@ -1,15 +1,47 @@
 import React, { useState } from 'react';
 import Header from '../elements/Header';
 import Menu from '../elements/Menu';
+import projects from '../data/projects';
+import './Projects.css';
 
 const Projects = () => {
   const [showMenu, changeMenuStatus] = useState(false);
+  const [index, changeIndex] = useState(0);
+  const changeProject = (signal) => {
+    switch (signal) {
+      case '+':
+        if (index === projects.length - 1) {
+          changeIndex(0);
+          break;
+        }
+        changeIndex(index + 1);
+        break;
+      case '-':
+        if (index === 0) {
+          changeIndex(projects.length - 1);
+          break;
+        }
+        changeIndex(index - 1);
+        break;
+      default: break;
+    }
+  };
+  const { name, description } = projects[index];
   return (
     <div>
       <Header title="Projects" menu={{ showMenu, changeMenuStatus }} />
       <Menu showMenu={showMenu} />
+      <div id="project-details">
+        <h1>{`Projeto ${name}`}</h1>
+        <h4>{`Detalhes: ${description}`}</h4>
+        <button type="button" onClick={() => changeProject('+')}>
+          Próximo Projeto
+        </button>
+        <button type="button" onClick={() => changeProject('-')}>
+          Projeto Anterior
+        </button>
+      </div>
     </div>
-
   );
 };
 
